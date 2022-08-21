bf09b45f48f5f18ed334e8e3b22f632cbea9bfc8
reasat-rafio <rafioar96@gmail.com>
2022-08-21 19:52:02 +0600

    :hammer: list update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const logOutBtn = document.querySelector(".log_out_warpper");
+const dashboardStatusContainer = document.querySelector(".dashboard > .status");
 
 logOutBtn.addEventListener("click", (e) => {
   e.preventDefault();
